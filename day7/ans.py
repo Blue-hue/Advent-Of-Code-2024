c22dcfd7690d082ba129f921bafb392e6cb8c0cc
Saptarshi Ghosh <ghoshsap2612@gmail.com>
2024-12-07 11:12:14 +0530

    just checking time lol

--- day7/ans.py
+++ day7/ans.py
@@ -1,3 +1,6 @@
+import time
+start = time.time()
+
 f = open("day7\\input.txt", "r").read().strip().split('\n')
 
 def satisfy(nums, i, n, memo=None):
@@ -5,16 +8,16 @@ def satisfy(nums, i, n, memo=None):
         memo = {}
     if i == n - 1:
         return [nums[i]]
-    if (i, n) in memo:
-        return memo[(i, n)]
+    if i in memo:
+        return memo[i]
 
     next_results = satisfy(nums, i + 1, n, memo)
     mulval = [nums[i] * x for x in next_results]
     addval = [nums[i] + x for x in next_results]
     concatval = [x * pow(10,len(str(nums[i]))) + nums[i] for x in next_results]
     
-    memo[(i, n)] = mulval + addval + concatval
-    return memo[(i, n)]
+    memo[i] = mulval + addval + concatval
+    return memo[i]
 
 ans1 = 0
 
@@ -22,8 +25,9 @@ for line in f:
     testno, nums = line.split(':')
     testno = int(testno)
     nums = list(map(int, nums.split()))
-    print(testno, nums)
+    # print(testno, nums)
     if testno in satisfy(nums[::-1], 0, len(nums)):
         ans1 += testno
 
 print(ans1)
+print("\n --- executed in :", time.time()-start, "seconds --- ")
